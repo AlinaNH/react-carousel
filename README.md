3d29f28d021ad4fd55c10b337fe41a474183827f
AlinaNH <anazarovich.alina@gmail.com>
2020-11-24 18:45:09 +0300

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+## React Carousel
+
+React.js adaptive component Carousel (slideshow)
+
+
+## Installation
+
+```
+npm install
+```
+
+
+## Running the app
+
+```
+npm run start
+```
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const webpack = require("webpack");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js"
+  },
+  mode: 'development',
+  devServer: {
+      historyApiFallback: true,
+      contentBase: path.resolve(__dirname, './dist'),
+      open: true,
+      compress: true,
+      hot: true,
+      port: 8080,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HTMLWebpackPlugin({
+      template: "./src/index.html"
+    }),
+    new CleanWebpackPlugin()
+  ]
+}
